298e6aa95070d161dbc1c2c73594e0605598f1fc
Pooja Wahatule <poojawahatule9@gmail.com>
2025-12-18 21:44:52 +0530

    Initial commit – Academic Calendar Android App

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()           // REQUIRED for com.android.application
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ACADEMICCALENDAR"
+include(":app")
